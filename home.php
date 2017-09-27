d95e0f5fdf046cbfe6f0c61c4ab6adde722ef60c
peash02 <Shaun.Pearce@contractor.ca.com>
2017-09-26 20:09:57 -0700

    Restructued and added home instead of index

---
+++ home.php
@@ -0,0 +1,25 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/css/rough-css.css">
+</head>
+
+<body>
+    
+    <?php include ( './components/nav.php' ) ; ?>
+    
+    <div class="page">
+        <div class="content">
+
+
+        </div>
+    </div>
+
+</body>
+</html>
