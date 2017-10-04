54556e14192e2352c7639c510278c0479b3e2c30
Shaun Pearce <shaunpearce1992@gmail.com>
2017-10-04 01:28:32 -0700

    Adding svg's to header

--- components/home-header.php
+++ components/home-header.php
@@ -1,5 +1,24 @@
 <div class="home-header-container page-header-container">
-    <div class="home-header page-header">
-        <h1>Header</h1>
+    <div class="home-header-background page-header-background"></div>    
+    
+    <div class="home-header-content header-content">
+
+        <div class="home-header-top-logos-container">
+            <div class="feel-better-logo-container">
+                <img src="assets/img/feel-better-logo-white.svg">
+            </div>
+            <div class="act-better-logo-container">
+                <img src="assets/img/act-better-logo-white9.svg">
+            </div>
+        </div>
+
+        
+        <div class="home-header-logo">
+            <img src="assets/img/think-better-logo-white.svg">
+        </div>
+
+
     </div>
+
+    
 </div>
