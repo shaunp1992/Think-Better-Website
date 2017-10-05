055f6f03e00a8dcbf9a9140fd6105de7c3083d25
Shaun Pearce <shaunpearce1992@gmail.com>
2017-10-05 00:38:50 -0700

    Completed home header

--- components/home-header.php
+++ components/home-header.php
@@ -1,22 +1,40 @@
 <div class="home-header-container page-header-container">
     <div class="home-header-background page-header-background"></div>    
     
-    <div class="home-header-content header-content">
+    <div class="home-header-content header-content content">
 
+        <div class="page-header-word-background">
+            <div class="header-background-word">Mindfulness</div>
+            <div class="header-background-word">Refocus</div>
+            <div class="header-background-word">Self Confidence</div>
+            <div class="header-background-word">Relax</div>
+            <div class="header-background-word">Refresh</div>
+            <div class="header-background-word">Best Self</div>
+        </div>
+        
         <div class="home-header-top-logos-container">
             <div class="feel-better-logo-container">
-                <img src="assets/img/feel-better-logo-white7.svg">
+                <img src="assets/img/feel-better-logo-white-shadow.svg">
             </div>
             <div class="act-better-logo-container">
-                <img src="assets/img/act-better-logo-white5.svg">
+                <img src="assets/img/act-better-logo-white-shadow.svg">
             </div>
         </div>
 
         
-        <div class="home-header-logo">
-            <img src="assets/img/think-better-logo-white4.svg">
+        <div class="home-header-logo-container">
+            <img src="assets/img/think-better-logo-white-shadow.svg">
         </div>
 
+        <div class="home-header-subtitle header-subtitle">
+            <span class="home-header-subtitle-line-one">Learn everyday tools to help you be...</span>
+            <span class="home-header-subtitle-line-two">The best version of you</span>
+        </div>
+        
+        <div class="home-header-button button button-white">
+            <div class="button-text button-text-purple">Learn More</div>
+        </div>
+        
     </div>
 
     
