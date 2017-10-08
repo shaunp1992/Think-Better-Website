f12ddd89fbcb9806dfc68b181aa244b4db44dddc
Shaun Pearce <shaunpearce1992@gmail.com>
2017-10-08 09:46:37 -0700

    Chaing navbar paths

--- components/navbar.php
+++ components/navbar.php
@@ -6,19 +6,19 @@
             </div>
 
             <div class="nav-bar-items-container desktop-menu">
-                <div class="nav-item"><a href="/home.php">Home</a></div>
+                <div class="nav-item"><a href="home.php">Home</a></div>
                 <div class="nav-item nav-item-dropdown">
                     <div class="nav-item-dropdown-text">Services</div>
                     <div class="nav-item-dropdown-container">
                         <div class="nav-item-dropdown-item" ><a href="workshop.php">Think Better Workshops</a></div>
-                        <div class="nav-item-dropdown-item"><a href="/work-workshop.php">Think Better Workshops at Work</a></div>
-                        <div class="nav-item-dropdown-item"><a href="/coaching.php">Cognitive Beahaviour Coaching</a></div>
-                        <div class="nav-item-dropdown-item"><a href="/therapy.php">Cognitive Behaviour Therapy</a></div>
+                        <div class="nav-item-dropdown-item"><a href="work-workshop.php">Think Better Workshops at Work</a></div>
+                        <div class="nav-item-dropdown-item"><a href="coaching.php">Cognitive Beahaviour Coaching</a></div>
+                        <div class="nav-item-dropdown-item"><a href="therapy.php">Cognitive Behaviour Therapy</a></div>
                     </div>
                 </div>
-                <div class="nav-item"><a href="/about.php">About</a></div>
-                <div class="nav-item"><a href="/blog.php">Blog</a></div>
-                <div class="nav-item"><a href="/contact.php">Contact</a></div>
+                <div class="nav-item"><a href="about.php">About</a></div>
+                <div class="nav-item"><a href="blog.php">Blog</a></div>
+                <div class="nav-item"><a href="contact.php">Contact</a></div>
             </div>
             
             <div class="mobile-menu-button mobile-menu">
@@ -30,15 +30,15 @@
                 <div class="nav-item">Services</div>
                 <div class="nav-item-dropdown">
                     <div class="nav-item-dropdown-container">
-                         <div class="nav-item-dropdown-item" ><a href="/workshop.php">Think Better Workshops</a></div>
-                        <div class="nav-item-dropdown-item"><a href="/work-workshop.php">Think Better Workshops at Work</a></div>
-                        <div class="nav-item-dropdown-item"><a href="/coaching.php">Cognitive Beahaviour Coaching</a></div>
-                        <div class="nav-item-dropdown-item"><a href="/therapy.php">Cognitive Behaviour Therapy</a></div>
+                         <div class="nav-item-dropdown-item" ><a href="workshop.php">Think Better Workshops</a></div>
+                        <div class="nav-item-dropdown-item"><a href="work-workshop.php">Think Better Workshops at Work</a></div>
+                        <div class="nav-item-dropdown-item"><a href="coaching.php">Cognitive Beahaviour Coaching</a></div>
+                        <div class="nav-item-dropdown-item"><a href="therapy.php">Cognitive Behaviour Therapy</a></div>
                     </div>
                 </div>
-                <div class="nav-item"><a href="/about.php">About</a></div>
-                <div class="nav-item"><a href="/blog.php">Blog</a></div>
-                <div class="nav-item"><a href="/contact.php">Contact</a></div>
+                <div class="nav-item"><a href="about.php">About</a></div>
+                <div class="nav-item"><a href="blog.php">Blog</a></div>
+                <div class="nav-item"><a href="contact.php">Contact</a></div>
             </div>
 
         </div>
