633d2c9bf1a258e8bf8bb54fc7aa17428b211e19
Shaun Pearce <shaunpearce1992@gmail.com>
2017-10-14 23:33:09 -0700

    Start of workshop page

---
+++ think-better-workshop.php
@@ -0,0 +1,63 @@
+<?php include('perch/runtime.php'); ?>
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Think Better Workshop</title>
+    <?php include ( './common/common-head.php' ) ; ?>
+</head>
+
+<body>
+    
+    <?php include ( './components/navbar.php' ) ; ?>
+    <?php perch_content('Header'); ?>
+    
+    <div class="page">
+        <div class="section">
+             <div class="content top-content">
+                <?php perch_content('Benefits Of The Workshop'); ?>
+            </div>
+        </div>
+            
+        <div class="section">
+            <div class="content">
+                <?php perch_content('The Day'); ?>
+            </div>
+        </div>
+        
+        <div class="section purple-section">
+             <div class="content">
+                <div class="section-title-container">
+                    <div class="section-title section-title-white">Upcoming Events</div>
+                    <div class="section-title-underline">
+                        <img src="assets/img/title-underline-white.svg">
+                    </div>
+                </div>
+                 
+                 <div class="events-container owl-carousel owl-theme">
+                        <?php include ( './components/event-card.php' ) ; ?>
+                        <?php include ( './components/event-card.php' ) ; ?>
+                        <?php include ( './components/event-card.php' ) ; ?>
+                        <?php include ( './components/event-card.php' ) ; ?>
+                 </div>
+                
+            </div>
+        </div>
+        
+        <div class="section subtle-purple-section">
+            <div class="content">
+                <?php perch_content('Booking Form'); ?>
+                <?php include ( './components/booking-form.php' ) ; ?>
+            </div>
+        </div>
+        
+    </div>
+    
+    <?php include ( './components/footer.php' ) ; ?>
+
+</body>
+    
+<?php include ( './common/common-body.php' ) ; ?>    
+</html>
