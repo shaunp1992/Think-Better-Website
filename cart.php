2c47fc24c15a3b594734ecb247025c5651eb0139
Shaun Pearce <shaunpearce1992@gmail.com>
2017-10-15 16:54:39 -0700

    Adding cart page

---
+++ cart.php
@@ -0,0 +1,32 @@
+<?php include('perch/runtime.php'); ?>
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Think Better Workshops</title>
+    <?php include ( './common/common-head.php' ) ; ?>
+</head>
+
+<body>
+    
+    <?php include ( './components/navbar.php' ) ; ?>
+    <?php perch_content('Header'); ?>
+    
+    <div class="page">
+        <div class="section">
+             <div class="content top-content">
+                <?php perch_shop_cart();?>
+            </div>
+        </div>
+        
+        
+    </div>
+    
+    <?php include ( './components/footer.php' ) ; ?>
+
+</body>
+    
+<?php include ( './common/common-body.php' ) ; ?>    
+</html>
