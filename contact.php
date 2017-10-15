a8e3fe64e91313642d28d9d21f2568fc2b1f2594
Shaun Pearce <shaunpearce1992@gmail.com>
2017-10-14 22:56:02 -0700

    Adding contact page

---
+++ contact.php
@@ -0,0 +1,38 @@
+<?php include('perch/runtime.php'); ?>
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <?php include ( './common/common-head.php' ) ; ?>
+</head>
+
+<body>
+    
+    <?php include ( './components/navbar.php' ) ; ?>
+    <?php perch_content('Header'); ?>
+    
+    <div class="page">
+        <div class="section">
+             <div class="content">
+                <?php perch_content('Contact Details'); ?>
+            </div>
+        </div>
+            
+        <div class="section subtle-purple-section">
+            <div class="content">
+                <?php perch_content('Contact Form'); ?>
+                <?php include ( './components/contact-form.php' ) ; ?>
+            </div>
+        </div>
+        
+    </div>
+    
+    <?php include ( './components/footer.php' ) ; ?>
+
+</body>
+    
+<?php include ( './common/common-body.php' ) ; ?>    
+</html>
