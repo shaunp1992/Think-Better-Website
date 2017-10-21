5500e7f5777c2ef3f95e43479d7d1947a245eb3d
Shaun Pearce <shaunpearce1992@gmail.com>
2017-10-20 18:23:31 -0700

    Start of blog

---
+++ blog.php
@@ -0,0 +1,34 @@
+<?php include('perch/runtime.php'); ?>
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Think Better Blog</title>
+    <?php include ( './common/common-head.php' ) ; ?>
+</head>
+
+<body>
+    
+    <?php include ( './components/navbar.php' ) ; ?>
+    <?php perch_content('Header'); ?>
+    
+    <div class="page">
+        <div class="section">
+             <div class="content">
+		      <?php perch_blog_recent_posts(10);?>
+		      <p><a href="archive.php">More posts</a></p>
+            </div>
+        </div>
+            
+       
+        
+    </div>
+    
+    <?php include ( './components/footer.php' ) ; ?>
+
+</body>
+    
+<?php include ( './common/common-body.php' ) ; ?>    
+</html>
