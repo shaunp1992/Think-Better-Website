dc412eaee5520210aa92c7e4bdda85a6cfe7d8ab
Shaun Pearce <shaunpearce1992@gmail.com>
2017-10-21 01:58:26 -0700

    Adding check for anchor

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,29 +23,26 @@ $(document).ready(function(){
     });
     
     $("a").on('click', function(event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
 
-            // Store hash
-            var hash = this.hash;
+    // Make sure this.hash has a value before overriding default behavior
+        console.log(this.offset());
+    if (this.hash !== "" && this.offset()) {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            if($(hash).offset().top){
+      // Store hash
+      var hash = this.hash;
 
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-              }, 800, function(){
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-              });
-
-            }
-          
-        }
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
         
     jQuery.fn.exists = function(){return this.length>0;};
