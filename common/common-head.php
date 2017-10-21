f8b61d22f2a5bade3b43d5a8b717553db91cd6e5
Shaun Pearce <shaunpearce1992@gmail.com>
2017-10-20 23:23:23 -0700

    Changing common header path

--- common/common-head.php
+++ common/common-head.php
@@ -1,6 +1,6 @@
 <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700|Open+Sans+Condensed:300,700" rel="stylesheet">
-<link rel="stylesheet" href="./assets/css/main.css">
+<link rel="stylesheet" href="/assets/css/main.css">
 
-<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
-<link rel="stylesheet" href="assets/css/magnific-popup.css">
+<link rel="stylesheet" href="/assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="/assets/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="/assets/css/magnific-popup.css">
